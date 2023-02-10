b9971cb96e020d50e4a81ebc5e456c20b668ed02
Cago75 <88317757+Cago75@users.noreply.github.com>
2023-02-10 23:02:02 +0200

    Added movement and animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,16 +7,23 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
     Rigidbody2D rigidBody;
-    [SerializeField]float velocity = 10;
+    [SerializeField] float velocity     = 10f;
+    [SerializeField] float jumpSpeed    = 10f;
+    [SerializeField] float climbSpeeds  = 10f;
+    Animator animator;
+    CapsuleCollider2D capsuleCollider;
 
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody   = GetComponent<Rigidbody2D>();
+        animator    = GetComponent<Animator>();
+        capsuleCollider    = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
     {
         Run();
+        ClimbLadder();
         FlipSprite();
     }
 
@@ -28,8 +35,38 @@ public class PlayerMovement : MonoBehaviour
 
     void Run()
     {
+       
         Vector2 playerVelocity = new Vector2(moveInput.x * velocity, rigidBody.velocity.y);
         rigidBody.velocity = playerVelocity;
+
+        bool playerHorizontalSpeed = Mathf.Abs(rigidBody.velocity.x) > Mathf.Epsilon; 
+        bool playerVerticalSpeed = Mathf.Abs(rigidBody.velocity.y) > 0.1; 
+        animator.SetBool("isJumping", (playerVerticalSpeed));
+        animator.SetBool("isRunning", playerHorizontalSpeed&&!playerVerticalSpeed);        
+    }
+    void OnJump(InputValue value)
+    {
+        //bool playerVerticalSpeed = Mathf.Abs(rigidBody.velocity.y) > Mathf.Epsilon; 
+        if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {return;}
+        
+        if(value.isPressed)
+        { 
+            rigidBody.velocity += new Vector2(0f, jumpSpeed);
+              
+        }
+        animator.SetBool("isJumping", true); 
+    }
+    void ClimbLadder()
+    {
+        if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {return;}
+
+        Vector2 ClimbVelocity = new Vector2(rigidBody.velocity.x, moveInput.y * climbSpeeds);
+        rigidBody.velocity = ClimbVelocity;
+        
+
+
     }
 
     void FlipSprite()
@@ -39,7 +76,8 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector2(Mathf.Sign(rigidBody.velocity.x), 1f);
         } 
-        
-        
+               
     }
+
+
 }
